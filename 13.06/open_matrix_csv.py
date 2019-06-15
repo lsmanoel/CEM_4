f74609bfd6da2155cfec7e53aea2d70974ecd685
Lucas Seara Manoel <ls.manoel@gmail.com>
2019-06-15 00:43:18 -0300

    update

--- 13.06/open_matrix_csv.py
+++ 13.06/open_matrix_csv.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 url_elc_plane_bottom_24_MHz = 'https://raw.githubusercontent.com/lsmanoel/CEM_4/master/13.06/placaL_medidas%20-%20%20plane_bottom_24_MHz.csv'
 url_elc_plane_bottom_500_KHz = 'https://raw.githubusercontent.com/lsmanoel/CEM_4/master/13.06/placaL_medidas%20-%20plane_bottom_500_KHz.csv'
@@ -29,8 +28,24 @@ elc_plane_top_500_KHz = -elc_plane_top_500_KHz
 mag_plane_bottom_24_MHz = -mag_plane_bottom_24_MHz
 mag_plane_top_24_MHz = -mag_plane_top_24_MHz
 
-plt.figure(figsize=(9,9))
-plt.imshow(mag_plane_bottom_24_MHz, cmap='inferno')
-plt.grid(False)
+
+# ==============================================================================
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(11,8))
+
+# ==============================================================================  
+ax = fig.add_subplot(1, 1, 1)
+
+img_elc_plane_top_24_MHz = ax.imshow(elc_plane_top_24_MHz, 
+                                     interpolation ='gaussian', 
+                                     cmap ='inferno',
+                                     vmax = 0,
+                                     vmin = -80)
+
+fig.colorbar(img_elc_plane_top_24_MHz, ax=ax)
+
+ax.grid(False)
 
 plt.show()
